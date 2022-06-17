93b6e3245265bc201a769a8cc5b70b784e2c7613
daneielle-pont <danielleshereepont@outlook.com>
2022-06-17 22:46:17 +1200

    temporary img sizing

--- src/compnents/Content.jsx
+++ src/compnents/Content.jsx
@@ -1,7 +1,7 @@
 function Content() {
   return (
     <>
-      <div className="Content">
+      <div className="content">
       <h1>CONTENT COMPONENT</h1>
         <img src="/images/header.png" alt='woman smiling at camera' />
       </div>
