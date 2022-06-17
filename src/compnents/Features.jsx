e60f9443004c263cdf7296228784d481c3c13aa7
daneielle-pont <danielleshereepont@outlook.com>
2022-06-17 22:54:31 +1200

    initial setout for Features

---
+++ src/compnents/Features.jsx
@@ -0,0 +1,14 @@
+function Features() {
+  return (
+    <>
+      <div className="features">
+      <h1>FEATURES COMPONENT</h1>
+        {/* <img src="/images/name.png" alt='name' /> */}
+      </div>
+      <div className="main">
+      </div>
+    </>
+  );
+}
+
+export default Features
