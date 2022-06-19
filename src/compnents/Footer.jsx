293eaac86417f83b4b80bf9d096a4d94f5d9f8e1
daneielle-pont <danielleshereepont@outlook.com>
2022-06-19 22:27:28 +1200

    add link names

--- src/compnents/Footer.jsx
+++ src/compnents/Footer.jsx
@@ -12,19 +12,52 @@ function Footer() {
       <ul className='company'>
         <NavLink className="links" to="/"><b>Company</b></NavLink>
           <li>
-            <NavLink className="links" to="/">Company</NavLink>
+            <NavLink className="links" to="/">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink className="links" to="/">Pricing</NavLink>
+          </li>
+          <li>
+            <NavLink className="links" to="/">Careers</NavLink>
           </li>
       </ul>
       <ul>
         <NavLink className="links" to="/"><b>Features</b></NavLink>
           <li>
-          <NavLink className="links" to="/">Features</NavLink>
+          <NavLink className="links" to="/">Relab Planning Report(RPR)</NavLink>
+          </li>
+          <li>
+          <NavLink className="links" to="/">Property Summary Report</NavLink>
+          </li>
+          <li>
+          <NavLink className="links" to="/">Advanced Mapping</NavLink>
+          </li>
+          <li>
+          <NavLink className="links" to="/">Building and Resource Consent</NavLink>
+          </li>
+          <li>
+          <NavLink className="links" to="/">Advanced Insights(ASI)</NavLink>
           </li>
       </ul>
       <ul>
         <NavLink className="links" to="/"><b>Resources</b></NavLink>
           <li>
-            <NavLink className="links" to="/">Resources</NavLink>
+            <NavLink className="links" to="/">Product Training</NavLink>
+          </li>
+          <li>
+            <NavLink className="links" to="/">Property Collective</NavLink>
+          </li>
+          <li>
+            <NavLink className="links" to="/">Property Webinars</NavLink>
+          </li>
+          <li>
+            <NavLink className="links" to="/">Product News</NavLink>
+          </li>
+          <li>
+            <NavLink className="links" to="/">Book a Demo</NavLink>
+          </li>
+          <li>
+            <NavLink className="links" to="/">Blog</NavLink>
           </li>
       </ul>
       </div>
