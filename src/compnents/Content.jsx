64174f994127ad4a1f0b1c4194f9905238897dd1
daneielle-pont <danielleshereepont@outlook.com>
2022-06-20 15:56:31 +1200

    benefit list positioning

--- src/compnents/Content.jsx
+++ src/compnents/Content.jsx
@@ -15,11 +15,18 @@ function Content() {
       </div>
       <div className='content'>
         <div className='benefitsList'>
-          <ol className='benefits'>
-              <li><p className='benefitTitle'>Search all in one place</p> Skip jumping from site to site to find what you need. Access all your property info in one easy to navigate platform.</li>
-              <li><p className='benefitTitle'>Find property opportunities faster</p>Find the best deals before anyone else. Save time on admin and outsmart your competition.</li>
-              <li><p className='benefitTitle'>Uncover hidden gems</p>Search using your own unique parameters and exact specifications, delivering you bespoke results that will set you apart from your competitors.</li>
-          </ol>
+          <div className='benefit01'>
+            <p className='benefitNumber'>01.</p>
+              <p><p className='benefitTitle'>Search all in one place</p>Skip jumping from site to site to find what you need. Access all your property info in one easy to navigate platform.</p>
+          </div>
+          <div className='benefit02'>
+            <p className='benefitNumber'>02.</p>
+              <p><p className='benefitTitle'>Find property opportunities faster</p>Find the best deals before anyone else. Save time on admin and outsmart your competition.</p>
+          </div>
+          <div className='benefit03'>
+            <p className='benefitNumber'>03.</p>
+              <p><p className='benefitTitle'>Uncover hidden gems</p>Search using your own unique parameters and exact specifications, delivering you bespoke results that will set you apart from your competitors.</p>
+          </div>
         </div>
       </div>
     </>
