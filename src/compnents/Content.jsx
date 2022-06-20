4af9064954d43ef6d8e414bf07ff2f292c160a80
daneielle-pont <danielleshereepont@outlook.com>
2022-06-20 16:28:46 +1200

    list number positioning

--- src/compnents/Content.jsx
+++ src/compnents/Content.jsx
@@ -3,6 +3,7 @@ import '../styling/content.css';
 function Content() {
   return (
     <>
+    <div className='Content'>
       <div className='banner'>
         <div className='statement'>
           <h1 className='slogan'>Find <u className='boldText'><b>unique</b></u> opportunities, <u className='boldText'><b>faster</b></u></h1>
@@ -10,22 +11,23 @@ function Content() {
         </div>
         <img src="/images/header.png" alt='woman smiling at camera' />
       </div>
-      <div className='title'>
-          <p>Benefits</p>
-      </div>
-      <div className='content'>
-        <div className='benefitsList'>
-          <div className='benefit01'>
-            <p className='benefitNumber'>01.</p>
-              <p><p className='benefitTitle'>Search all in one place</p>Skip jumping from site to site to find what you need. Access all your property info in one easy to navigate platform.</p>
-          </div>
-          <div className='benefit02'>
-            <p className='benefitNumber'>02.</p>
-              <p><p className='benefitTitle'>Find property opportunities faster</p>Find the best deals before anyone else. Save time on admin and outsmart your competition.</p>
-          </div>
-          <div className='benefit03'>
-            <p className='benefitNumber'>03.</p>
-              <p><p className='benefitTitle'>Uncover hidden gems</p>Search using your own unique parameters and exact specifications, delivering you bespoke results that will set you apart from your competitors.</p>
+        <div className='title'>
+            <p>Benefits</p>
+        </div>
+        <div className='content'>
+          <div className='benefitsList'>
+            <div className='benefit'>
+              <p className='benefitNumber'><p className='numberTitle'>Benefits</p>01.</p>
+                <p><p className='benefitTitle'><b>Search all in one place</b></p>Skip jumping from site to site to find what you need. Access all your property info in one easy to navigate platform.</p>
+            </div>
+            <div className='benefit'>
+              <p className='benefitNumber'><p className='numberTitle'>Benefits</p>02.</p>
+                <p><p className='benefitTitle'><b>Find property opportunities faster</b></p>Find the best deals before anyone else. Save time on admin and outsmart your competition.</p>
+            </div>
+            <div className='benefit'>
+              <p className='benefitNumber'><p className='numberTitle'>Benefits</p>03.</p>
+                <p><p className='benefitTitle'><b>Uncover hidden gems</b></p>Search using your own unique parameters and exact specifications, delivering you bespoke results that will set you apart from your competitors.</p>
+            </div>
           </div>
         </div>
       </div>
